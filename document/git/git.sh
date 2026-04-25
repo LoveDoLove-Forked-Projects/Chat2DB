0e7ff0a67894efc43a16e0171624bcd1a99f8476
robinji9102 <162428821+robinji9102@users.noreply.github.com>
2026-04-25 20:15:12 +0800

    Update git.sh
    Signed-off-by: robinji9102 <162428821+robinji9102@users.noreply.github.com>

--- document/git/git.sh
+++ document/git/git.sh
@@ -2,6 +2,7 @@
 # -r represents recursion
 #
 #
+#
 git filter-branch --force --index-filter 'git rm -r --cached --ignore-unmatch chat2db-client/static' --prune-empty --tag-name-filter cat -- --all
 git push origin developing  --force
 git push origin delete_git  --force
