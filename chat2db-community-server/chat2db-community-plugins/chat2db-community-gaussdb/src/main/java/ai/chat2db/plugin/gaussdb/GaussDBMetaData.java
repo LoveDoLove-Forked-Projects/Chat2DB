c7d0b2177c366b3c28a21794536997f471576a2e
zgq <openai0229@gmail.com>
2026-07-20 15:52:29 +0800

    fix(gaussdb): preserve comments in table DDL

--- chat2db-community-server/chat2db-community-plugins/chat2db-community-gaussdb/src/main/java/ai/chat2db/plugin/gaussdb/GaussDBMetaData.java
+++ chat2db-community-server/chat2db-community-plugins/chat2db-community-gaussdb/src/main/java/ai/chat2db/plugin/gaussdb/GaussDBMetaData.java
@@ -10,17 +10,69 @@ import org.apache.commons.lang3.StringUtils;
 import java.sql.Connection;
 import java.util.List;
 
+import static ai.chat2db.plugin.gaussdb.constant.GaussDBMetaDataConstants.SEARCH_PATH_STATEMENT_PREFIX;
+import static ai.chat2db.plugin.gaussdb.constant.GaussDBMetaDataConstants.TABLE_DDL_SQL;
+
 @Slf4j
 public class GaussDBMetaData extends PostgreSQLMetaData implements IDbMetaData {
     @Override
     public String tableDDL(Connection connection, String databaseName, String schemaName, String tableName) {
-        String sql = "SELECT pg_get_tabledef('" + tableName + "')";
-        return DefaultSQLExecutor.getInstance().execute(connection, sql, resultSet -> {
-            if (resultSet.next()) {
-                return resultSet.getString(1);
-            }
+        String ddl = DefaultSQLExecutor.getInstance().preExecute(connection, TABLE_DDL_SQL,
+                new String[]{schemaName, tableName}, resultSet -> resultSet.next() ? resultSet.getString(1) : null);
+        return stripLeadingSearchPath(ddl);
+    }
+
+    static String stripLeadingSearchPath(String ddl) {
+        if (ddl == null) {
             return null;
-        });
+        }
+
+        int statementStart = 0;
+        while (statementStart < ddl.length() && Character.isWhitespace(ddl.charAt(statementStart))) {
+            statementStart++;
+        }
+
+        if (!ddl.regionMatches(true, statementStart, SEARCH_PATH_STATEMENT_PREFIX, 0,
+                SEARCH_PATH_STATEMENT_PREFIX.length())) {
+            return ddl;
+        }
+
+        int prefixEnd = statementStart + SEARCH_PATH_STATEMENT_PREFIX.length();
+        if (prefixEnd < ddl.length() && Character.isJavaIdentifierPart(ddl.charAt(prefixEnd))) {
+            return ddl;
+        }
+
+        int statementEnd = findStatementEnd(ddl, prefixEnd);
+        if (statementEnd < 0) {
+            return ddl;
+        }
+
+        int ddlStart = statementEnd + 1;
+        while (ddlStart < ddl.length() && Character.isWhitespace(ddl.charAt(ddlStart))) {
+            ddlStart++;
+        }
+        return ddl.substring(ddlStart);
+    }
+
+    private static int findStatementEnd(String sql, int start) {
+        char quote = 0;
+        for (int i = start; i < sql.length(); i++) {
+            char current = sql.charAt(i);
+            if (quote != 0) {
+                if (current == quote) {
+                    if (i + 1 < sql.length() && sql.charAt(i + 1) == quote) {
+                        i++;
+                    } else {
+                        quote = 0;
+                    }
+                }
+            } else if (current == '\'' || current == '"') {
+                quote = current;
+            } else if (current == ';') {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
